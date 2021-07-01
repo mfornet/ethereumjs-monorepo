3afda54204cb9d0258158afe14cacf8ad02d1e4a
Marcelo Fornet <mfornet94@gmail.com>
2021-07-01 06:01:11 +0200

    Compile ethereumjs/block and add it to GitHub

---
+++ packages/block/dist/clique.d.ts
@@ -0,0 +1,5 @@
+import { BN } from 'ethereumjs-util';
+export declare const CLIQUE_EXTRA_VANITY = 32;
+export declare const CLIQUE_EXTRA_SEAL = 65;
+export declare const CLIQUE_DIFF_INTURN: BN;
+export declare const CLIQUE_DIFF_NOTURN: BN;
